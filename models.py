0aed891142c627725212f7d55f720df79527c452
VampireTeeth <vampire001@gmail.com>
2014-05-13 21:49:45 +1000

    Comment and CommentTest added

--- models.py
+++ models.py
@@ -1,21 +1,35 @@
-from mongoengine import Document, StringField, DateTimeField, ReferenceField, ObjectIdField
+import mongoengine as mg
 import bson
+from datetime import datetime
 
 
-class User(Document):
-    id = ObjectIdField(primary_key=True, default=lambda:bson.ObjectId())
-    email = StringField(max_length=50)
-    first_name = StringField(max_length=50)
-    last_name = StringField(max_length=50)
+class User(mg.Document):
+    id = mg.ObjectIdField(primary_key=True, default=lambda:bson.ObjectId())
+    email = mg.StringField(max_length=50)
+    first_name = mg.StringField(max_length=50)
+    last_name = mg.StringField(max_length=50)
 
 
-class Login(Document):
-    username = StringField(max_length=50)
-    password = StringField(max_length=32)
+class Login(mg.Document):
+    username = mg.StringField(max_length=50)
+    password = mg.StringField(max_length=32)
 
 
-class Blog(Document):
-    id = ObjectIdField(primary_key=True, default=lambda:bson.ObjectId())
-    dateTime = DateTimeField(required=True)
-    content = StringField(max_length=1000)
-    author = ReferenceField('User')
+
+class Article(mg.Document):
+    id = mg.ObjectIdField(primary_key=True, default=lambda:bson.ObjectId())
+    dateTime = mg.DateTimeField(required=True, default=lambda:datetime.now())
+    content = mg.StringField(max_length=1000)
+    voted = mg.IntField(default=0)
+    author = mg.ReferenceField('User', required=True)
+
+
+class Comment(mg.Document):
+    id = mg.ObjectIdField(primary_key=True, default=lambda:bson.ObjectId())
+    dateTime = mg.DateTimeField(required=True, default=lambda:datetime.now())
+    content = mg.StringField(max_length=1000)
+
+class Vote(mg.Document):
+    #TODO
+    pass
+
--- test/ModelsTest.py
+++ test/ModelsTest.py
@@ -1,14 +1,17 @@
 import unittest, mongoengine, datetime, models
 
-class BlogTest(unittest.TestCase):
+
+
+MODELS = [models.Article, models.User, models.Comment]
+class BaseTest(unittest.TestCase):
 
     def setUp(self):
         mongoengine.connect('vampire-blog-test')
 
     def tearDown(self):
-        models.Blog.objects().delete()
-        models.User.objects().delete()
+        for model in MODELS: model.objects().delete()
 
+class ArticleTest(BaseTest):
 
     def testSave(self):
         u = models.User(first_name='Steven', last_name='Liu', email='steven.weike.liu@gmail.com')
@@ -17,14 +20,53 @@ class BlogTest(unittest.TestCase):
         for u in users:
             self.assertEqual(u.first_name, 'Steven')
 
-        b = models.Blog(dateTime=datetime.datetime.now(),
+        b = models.Article(dateTime=datetime.datetime.now(),
                 content='This is a sample blog content',
                 author=u)
         b.save()
 
-        bs = models.Blog.objects(author=u)
+        bs = models.Article.objects(author=u)
 
         for b in bs:
             self.assertEqual(b.author.first_name, 'Steven')
             self.assertEqual(b.content, 'This is a sample blog content')
+
+    def testSaveWithNonRequiredFieldsOnly(self):
+        u = models.User(first_name='Steven', last_name='Liu', email='steven.weike.liu@gmail.com')
+        u.save()
+
+        b = models.Article(author=u)
+        b.save()
+        bs = models.Article.objects(author=u)
+
+        for b in bs:
+            self.assertEqual(b.author.first_name, 'Steven')
+            self.assertEqual(b.content, None)
+
+    def testSaveManyBlogsWithSameUser(self):
+        u = models.User(first_name='Steven', last_name='Liu', email='steven.weike.liu@gmail.com')
+        u.save()
+
+        b1 = models.Article(author=u, content='Blog1')
+        b2 = models.Article(author=u, content='Blog2')
+
+        b1.save()
+        b2.save()
+
+        bs = models.Article.objects(author=u)
+        self.assertEqual(2, len(bs))
+
+
+        for b in bs:
+            self.assertIn(b.content, ['Blog1', 'Blog2'])
+
+class CommentTest(BaseTest):
+    def testSave(self):
+        content = 'haha this is a stupid comment'
+        c = models.Comment(content=content)
+        c.save()
+
+        comments = models.Comment.objects(content=content)
+        self.assertEqual(1, len(comments))
+        self.assertEqual(content, comments[0].content)
 

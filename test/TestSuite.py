154fdc1006e8147c00f5582cbd6393c330a4e19f
VampireTeeth <vampire001@gmail.com>
2014-05-13 21:47:37 +1000

    Comment and CommentTest aded

--- test/TestSuite.py
+++
@@ -1,9 +0,0 @@
-
-if __name__ == '__main__':
-    import sys, os.path
-    sys.path.insert(0, os.path.dirname((os.path.dirname(os.path.abspath(__file__)))))
-
-    import unittest, ModelsTest 
-
-    suite = unittest.TestLoader().loadTestsFromTestCase(ModelsTest.BlogTest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
